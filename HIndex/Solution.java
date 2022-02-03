636e7e8bfcef8a8d47f887832840f4022d4f64e7
choeseunghwan <dexrf1@gmail.com>
2022-02-03 23:46:15 +0900

    apply .gitignore

---
+++ HIndex/Solution.java
@@ -0,0 +1,22 @@
+package test.HIndex;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int len = citations.length;
+        int answer = 0;
+        int max = 0;
+
+        // 오름차순으로 정렬
+        Arrays.sort(citations);
+
+        for(int i= citations.length-1; i>=0 ; i--)
+        {
+            int min = Math.min(citations[i], len-i);
+            if(min > max) max = min;
+        }
+
+        return max;
+    }
+}
